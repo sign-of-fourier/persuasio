c05ff8afca699f6f0a6951e377c220d385932153
Mark Shipman <info@quantecarlo.com>
2025-04-03 16:31:27 +0000

    multiple vector dbs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='persuasio',
+      version='0.1.0',
+      description='Persuasive Chatbot',
+      url='https://persuas.io',
+      author='sign-of-fourier',
+      author_email='info@quantecarlo.com',
+      license='None',
+      packages=['persuasio'],
+      zip_safe=False)
