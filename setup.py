4381bc54e46722b7abe255654cca1dda719f763d
Mark Shipman <info@quantecarlo.com>
2025-04-03 19:01:20 +0000

    take out scipy and sklearn for now; seem to be memory issues

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='persuasio',
-      version='0.1.11',
+      version='0.1.12',
       description='Persuasive Chatbot',
       url='https://persuas.io',
       author='sign-of-fourier',
