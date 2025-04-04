a5b4075d3e3d0e4b3fbaa5e7ee7113f48b37b4b0
Mark Shipman <markpshipman@yahoo.com>
2025-04-04 12:19:59 -0400

    added html output

--- persuasio/app.py
+++ persuasio/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 
-import chromadb
+#import chromadb
 import pandas as pd
 import re
 import numpy as np 
@@ -21,82 +21,51 @@ import persuasio.agents as agents
 
 app = Flask(__name__)
 
+page = """
+<html>
+<p id='paragraph'>{}
+<form action='http://127.0.0.1:5000/persuasio?max_tokens=205&iteration=4&product_history_included=False' method=post>
+    <input type=hidden name=product_history value="{}"></input>
+    <input type=hidden name=system value='You are a sales person at Amazon.'></input>
+    <input type=hidden name=transcript_history value="{}"></input>
+    <input type=text name='user_statement'></input>
+    <input type=submit name=submissize value=nah></input></input>
+</form>
+</form>
+<p id='pics'><div style="height:700px; width700px;border:1px sold #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">{}</div>
 
+"""
 client = chromadb.PersistentClient(path="persuasio/chroma_small")
 client2 = chromadb.PersistentClient(path="persuasio/chroma_small2")
-
 description_db = client.get_collection(name="amazon_beauty_descriptions")
 description_db2 = client2.get_collection(name="amazon_beauty_descriptions2")
 
-page = """
-<html>
-<p>{}
-<form id=claude action='http://127.0.0.1:5000/claude?max_tokens=256' method=POST>
-<input type = 'hidden' name='history' rows=5 cols=80 value="{}"><br>
-<input id='prompt' name='prompt' type=text size=100></input>
-<input name='system' type='hidden' value='You are from New Jersey. Speak in a thick Jersey accent.'></input>
-<input type='submit' value='Submit'></input>
-</form>
-"""
 
-
-@app.route("/chromadb", methods = ['POST'])
-def chromadb():
-    
-    iteration = request.args.get('iteration')
-    data = json.loads(request.data)   
-    search_terms = data['search_terms']
-
-    results = description_db.query(
-        query_texts=search_terms,
-        n_results=3
-    )
-    results2 = description_db2.query(
-        query_texts=search_terms,
-        n_results=3
-    )
-    
-    search_df = pd.concat([utils.get_search_df(results, iteration), utils.get_search_df(results2, iteration)], axis=0)
-    return json.dumps(search_df.to_dict())
-
-
+reviews_df = pd.read_csv('persuasio/reviews.csv.gz', compression='gzip')
 
 
 @app.route("/persuasio", methods = ['POST'])
 def persuasio():
-    data = json.loads(request.data)   
-    transcript_history = data['transcript_history']
-    user_statement = data['user_statement']
-    product_history = data['product_history']
-    iteration = data['iteration']
-
-
-#    user_statment = request.form.get('user_statement')
-#    system = request.form.get('system')
-#    transcript_history = request.form.get('transcript_history')  
-#    product_history = request.form.get('product_history')
-
     
+    user_statement = request.form.get('user_statement')
+    system = request.form.get('system')
+    transcript_history = request.form.get('transcript_history')  
+    product_history = request.form.get('product_history')
+
+    iteration = request.form.get('iteration')    
     max_tokens = request.args.get('max_tokens')
     product_history_included = request.args.get('product_history_included')
 
-#    client = chromadb.PersistentClient(path="persuasio/chroma_small")
-#    client2 = chromadb.PersistentClient(path="persuasio/chroma_small2")
-#    description_db = client.get_collection(name="amazon_beauty_descriptions")
-#    description_db2 = client2.get_collection(name="amazon_beauty_descriptions2")
-    reviews_df = pd.read_csv('persuasio/reviews.csv.gz', compression='gzip')
 
     # greeting = 'Megan: Hi, my name is Megan. Is there something I can I help you with?'
 
-    conversation = transcript_history + "\nDasha: " + user_statement + "\n"
-
+    conversation = transcript_history + "\nDasha: " + user_statement 
 
     search_terms = agents.search(conversation)
     try:
         search_terms = eval(search_terms)
     except Exception as e:
         print(e)
-#    print(search_terms)
         
     results = description_db.query(
         query_texts=search_terms,
@@ -108,11 +77,15 @@ def persuasio():
     )
 
     search_df = pd.concat([utils.get_search_df(results, iteration), utils.get_search_df(results2, iteration)], axis=0)
+#    response = requests.post('https://persuasio.onrender.com/chromadb?iteration=1&max_tokens=250&include_product_history=False', data=json.dumps({'search_terms': search_terms}))
+#    search_df = pd.DataFrame(json.loads(response.content.decode('utf-8')))
+
     if product_history_included == 'True':
         search_df = pd.concat([search_df, pd.DataFrame(product_history)], axis=0) 
-    
     search_df = search_df.drop_duplicates(subset='id', inplace=False)
-    images = ["<img src='{}'></img>{}".format(i.split("\n")[0], t) for i, t in zip(search_df['image'], search_df['title'])]
+    #print("\n".join([i for i in search_df['image']]))
+    
+    images = ["<table><tr><td><img src='{}'></img></td></tr><tr><td><b>{}.</b> {}</td></tr></table>".format(i[1].split("\n")[0], i[0] + 1, t) for i, t in zip(enumerate(search_df['image']), search_df['title'])]
             #print(results['metadatas'][i][j]['title'], ', $' + str(results['metadatas'][i][j]['price']))
 
     product_info = ''
@@ -124,11 +97,11 @@ def persuasio():
         product_info += "Overall Average: {}\nNumber of Ratings: {}\nPrice: ${}\n\n".format(avg_rating, rating_n, price)
         ct += 1
 
-    reply = agents.salesman(conversation, product_info)
-    return json.dumps({'images': "\n".join(images), 
-                      'product_info': product_info, 'transcript_history': conversation + "\nMegan: " + reply,  'reply': reply,
-                      'product_history': search_df.to_dict()}) 
+    reply = re.sub("\n", ' ', agents.salesman(conversation, product_info))
 
+    transcript = conversation + "\nMegan: " + reply
+    return page.format(re.sub("\n", "<br>", re.sub('Dasha', '<b>Dasha</b>', re.sub('Megan', '<b>Megan</b>', transcript))), json.dumps(search_df.to_dict()),  transcript,  "<br>".join(images))
+    
 # conversation.append('Megan: ' + utils.remove_name('Megan', reply))
 # reply = input(reply)
 # conversation.append('Dasha: ' + utils.remove_name('Dasha', reply))
