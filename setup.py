8a32649382de111a8b601693b9de31414a607f92
Mark Shipman <markpshipman@yahoo.com>
2025-04-05 16:12:32 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='persuasio',
-      version='0.1.15',
+      version='0.2.0',
       description='Persuasive Chatbot',
       url='https://persuas.io',
       author='sign-of-fourier',
